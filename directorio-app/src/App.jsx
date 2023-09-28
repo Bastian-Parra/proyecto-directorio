465d62dbf303f7429dddd28304f0d304f46da2b7
Bastian-Parra <bastian.parra@gmail.com>
2023-09-28 12:55:05 -0300

    Add styles to navigator and app.jsx

--- directorio-app/src/App.jsx
+++ directorio-app/src/App.jsx
@@ -7,21 +7,22 @@ import Negocios from './components/Negocios.jsx'
 import Eventos from './components/Eventos.jsx'
 import Lugares from './components/Lugares.jsx'
 import Footer from './components/Footer.jsx'
+import './app.css'
 function App() {
   return (
     <div className="App">
       <header>
-        <Navegador/>
+        <Navegador/>     
       </header>
+      <div className="content">
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/negocios' element={<Negocios/>}></Route>
         <Route path='/eventos' element={<Eventos/>}></Route>
         <Route path='/lugares' element={<Lugares/>}></Route>
       </Routes>
-      <footer>
-        <Footer/>
-      </footer>
+      </div>
+      <Footer/>
     </div>
   )
 }
