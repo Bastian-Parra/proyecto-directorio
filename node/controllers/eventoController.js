a56c8de23432ed5f4e70ec3b88668b11ddec83a7
IgnacioGallardoAG <altrairg@gmail.com>
2023-11-27 00:15:14 -0300

    ultimo commit antes del merge

--- node/controllers/eventoController.js
+++ node/controllers/eventoController.js
@@ -21,65 +21,6 @@ export const obtenerEvento = async (req, res) => {
     res.json(evento)
 }
 
-export const eliminarEvento = async (req, res) => {
-    try {
-        const evento = await Evento.findByPk(req.params.id)
-        
-        if (!evento) return res.status(400).json({message: "Evento no encontrado"})
-        
-        await evento.destroy()
-        
-        res.status(200).json({ success: true, message: 'Evento eliminado exitosamente' });
-        
-    } catch (error) {
-        console.error('Error al eliminar un evento', error)
-        res.status(500).json({ success: false, error: 'Error al eliminar un evento'})
-    }
-}
-
-export const actualizarEvento = async (req, res) => {
-    try {
-        const evento = await Evento.findByPk(req.params.id)
-        
-        if (!evento) return res.status(400).json({message: "Evento no encontrado"})
-        
-        const {direccion_evento, nombre_evento, fecha_hora, descripcion_evento} = req.body;
-        
-        const eventoExistente = await verificarEvento(nombre_evento, direccion_evento)
-        
-        if (eventoExistente && eventoExistente.id !== evento.id) {
-            return res.status(400).json({message: "Evento encontrado con mismo nombre"})
-        }
-        
-        await evento.update({
-            direccion_evento: direccion_evento,
-            nombre_evento: nombre_evento,
-            descripcion_evento: descripcion_evento,
-            fecha_hora: fecha_hora,
-        })
-        
-        res.status(200).json({ success: true, message: 'Evento actualizado exitosamente' });
-        
-    } catch (error) {
-        console.error('Error al actualizar un evento', error)
-        res.status(500).json({ success: false, error: 'Error al actualizar un evento'})
-    }
-}
-
-export const crearEvento = async (direccion_evento, nombre_evento, fecha_hora, descripcion_evento) => {
-
-    try {
-        await Evento.create({
-          direccion_evento: direccion_evento,
-          nombre_evento: nombre_evento,
-          fecha_hora: fecha_hora,
-          descipcion_evento: descripcion_evento,
-        })
-    } catch (error) {
-        throw error
-    }
-}
-
 export const AgregarEvento = async (req, res) => {
     try {
         const {direccion_evento, nombre_evento, fecha_hora, descipcion_evento} = req.body;
@@ -100,13 +41,27 @@ export const AgregarEvento = async (req, res) => {
     }
 }
 
+export const crearEvento = async (direccion_evento, nombre_evento, fecha_hora, descripcion_evento) => {
+
+    try {
+        await Evento.create({
+          direccion_evento: direccion_evento,
+          nombre_evento: nombre_evento,
+          fecha_hora: fecha_hora,
+          descipcion_evento: descripcion_evento,
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
 export const verificarEvento = async (nombre_evento, direccion_evento) => {
     try {
         const evento = await Evento.findOne({
             where: {
               [Sequelize.Op.or]: [
-                { nombre: nombre_evento }, 
-                { direccion: direccion_evento },
+                { nombre_evento: nombre_evento }, 
+                { direccion_evento: direccion_evento },
               ],
             },
         })
@@ -116,7 +71,53 @@ export const verificarEvento = async (nombre_evento, direccion_evento) => {
     }
 }
 
-/*
+export const actualizarEvento = async (req, res) => {
+    try {
+        const evento = await Evento.findByPk(req.params.id)
+        
+        if (!evento) return res.status(400).json({message: "Evento no encontrado"})
+        
+        const {direccion_evento, nombre_evento, fecha_hora, descripcion_evento} = req.body;
+        
+        /*
+        const eventoExistente = await verificarEvento(nombre_evento, direccion_evento)
+        
+        if (eventoExistente && eventoExistente.id !== evento.id) {
+            return res.status(400).json({message: "Evento encontrado con mismo nombre"})
+        }
+        */
+        
+        await evento.update({
+            direccion_evento: direccion_evento,
+            nombre_evento: nombre_evento,
+            descripcion_evento: descripcion_evento,
+            fecha_hora: fecha_hora,
+        })
+        
+        res.status(200).json({ success: true, message: 'Evento actualizado exitosamente' });
+        
+    } catch (error) {
+        console.error('Error al actualizar un evento', error)
+        res.status(500).json({ success: false, error: 'Error al actualizar un evento'})
+    }
+}
+
+export const eliminarEvento = async (req, res) => {
+    try {
+        const evento = await Evento.findByPk(req.params.id)
+        
+        if (!evento) return res.status(400).json({message: "Evento no encontrado"})
+        
+        await evento.destroy()
+        
+        res.status(200).json({ success: true, message: 'Evento eliminado exitosamente' });
+        
+    } catch (error) {
+        console.error('Error al eliminar un evento', error)
+        res.status(500).json({ success: false, error: 'Error al eliminar un evento'})
+    }
+}
+
 export const AlmacenarImagenes = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "Images")
@@ -126,20 +127,4 @@ export const AlmacenarImagenes = multer.diskStorage({
     }
 })
 
-export const SubirImagenes = multer({
-    storage: AlmacenarImagenes,
-    limits: {fileSize: 1024 * 1024 * 10},
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif/
-        const mimetype = fileTypes.test(file.mimetype)
-        const extname = fileTypes.test(path.extname(file.originalname))
-
-        if (mimetype && extname) {
-            cb(null, true)
-        } else {
-            cb(null, false)
-        }
-    }
-}).single('imagen')
-
-*/
+export const SubirImagenes = multer({storage: AlmacenarImagenes})
--- node/models/eventosModel.js
+++ node/models/eventosModel.js
@@ -5,7 +5,8 @@ const Evento = db.define('eventos' , {
     
     direccion_evento: {type : DataTypes.STRING},
     nombre_evento: {type : DataTypes.STRING},
-    fecha_hora: {type : DataTypes.STRING},
+    fecha_hora: {type : DataTypes.DATE},
+    descripcion_evento: {type : DataTypes.STRING},
     
 }, {timestamps:false,})
 
